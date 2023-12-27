320517ae46c59c47c3fa21543ad2e5ae01cbbc69
acalvom <andreac.3108@gmail.com>
2023-12-27 16:51:34 +0100

    build: install cypress

---
+++ cypress.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'cypress'
+
+export default defineConfig({
+  chromeWebSecurity: false,
+  requestTimeout: 3000,
+  defaultCommandTimeout: 5000,
+  viewportWidth: 1500,
+  viewportHeight: 942,
+  numTestsKeptInMemory: 0,
+  retries: {
+    runMode: 1,
+    openMode: 2,
+  },
+  env: {
+    appUrl: 'http://localhost:3000',
+  },
+  e2e: {
+    testIsolation: true,
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+  },
+})
