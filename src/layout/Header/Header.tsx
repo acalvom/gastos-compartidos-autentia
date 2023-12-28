77623d5fc136d02e7c545a5888eaf9c58f48dda2
acalvom <andreac.3108@gmail.com>
2023-12-28 11:12:51 +0100

    test: unit test for Header component

--- src/layout/Header/Header.tsx
+++ src/layout/Header/Header.tsx
@@ -5,8 +5,10 @@ export const Header = () => {
   return (
     <header className="app-header">
       <nav className="app-navbar">
-        <h1 className="app-title">Gestión de gastos</h1>
-        <img className="app-logo" src={people} alt="People" />
+        <h1 className="app-title" data-testid="app-title">
+          Gestión de gastos
+        </h1>
+        <img className="app-logo" data-testid="app-logo" src={people} alt="People" />
       </nav>
     </header>
   )
