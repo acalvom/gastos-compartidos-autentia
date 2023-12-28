f3184f77c4c2d4459ea26c17ddd3a4ce9d4f4065
acalvom <andreac.3108@gmail.com>
2023-12-28 17:33:31 +0100

    fix: types

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -4,7 +4,7 @@ import Main from './Main/Main'
 import Footer from './Footer/Footer'
 
 type LayoutProps = {
-  children: JSX.Element[]
+  children: JSX.Element | JSX.Element[]
 }
 
 export const Layout = ({ children }: LayoutProps) => {
