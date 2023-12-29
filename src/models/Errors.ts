fca994c9a72b536137320800cfc8eef061c3906c
acalvom <andreac.3108@gmail.com>
2023-12-29 13:56:08 +0100

    feat: create model for errors

---
+++ src/models/Errors.ts
@@ -0,0 +1,11 @@
+export interface UserFormErrors {
+  firstName: string
+  lastName: string
+}
+
+export interface ExpenseFormErrors {
+  payer: string
+  amount: string
+  description: string
+  paymentDate: string
+}
