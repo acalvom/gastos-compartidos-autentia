834b64de21e646709023206e7a11b7aa772732d3
acalvom <andreac.3108@gmail.com>
2023-12-29 13:56:26 +0100

    feat: add StoreExpense model

--- src/models/Expense.ts
+++ src/models/Expense.ts
@@ -1,8 +1,11 @@
-import { StoredUser } from "./User"
+import { StoredUser } from './User'
 
-export interface Expense {
-  payer: StoredUser
+export interface StoredExpense {
+  id: string
+  payer: StoredUser["id"]
   description: string
   amount: number
   paymentDate?: string
 }
+
+export type Expense = Omit<StoredExpense, 'id'>
