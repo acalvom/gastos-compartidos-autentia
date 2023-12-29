d82dfed422557959b44b5414bda883d2c8185039
acalvom <andreac.3108@gmail.com>
2023-12-29 13:58:10 +0100

    feat: add expense form to Home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,19 @@
-import AddUserForm from '@/components/AddUserForm/AddUserForm'
 import './Home.css'
 import Layout from '@/layout/Layout'
+import { AddUserForm } from '@/components/AddUserForm/AddUserForm'
+import { AddExpenseForm } from '@/components/AddExpenseForm/AddExpenseForm'
+import { useLocalStorage } from '@/hooks/useLocalStorage'
+import { StoredUser } from '@/models/User'
+import { StoredExpense } from '@/models/Expense'
 
 export const Home = () => {
+  const [storedUsers, setStoredUsers] = useLocalStorage<StoredUser[]>('amigos', [])
+  const [storedExpenses, setStoredExpenses] = useLocalStorage<StoredExpense[]>('gastos', [])
 
   return (
     <Layout>
-      <AddUserForm />
+      <AddUserForm storedUsers={storedUsers} setStoredUsers={setStoredUsers} />
+      <AddExpenseForm storedUsers={storedUsers} storedExpenses={storedExpenses} setStoredExpenses={setStoredExpenses} />
     </Layout>
   )
 }
