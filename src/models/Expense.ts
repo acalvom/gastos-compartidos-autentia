f4f5f24c44fa1b00547615c1219123a78e0afb0c
acalvom <andreac.3108@gmail.com>
2023-12-31 10:13:11 +0100

    fix: field in Expenses model optional

--- src/models/Expense.ts
+++ src/models/Expense.ts
@@ -2,10 +2,10 @@ import { StoredUser } from './User'
 
 export interface StoredExpense {
   id: string
-  payer: StoredUser["id"]
+  payer: StoredUser['id']
   description: string
   amount: number
-  paymentDate?: string
+  paymentDate: string
 }
 
 export type Expense = Omit<StoredExpense, 'id'>
