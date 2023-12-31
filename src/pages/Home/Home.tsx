c8ca8b0fa2c1b9395862c451c438f2e7d76d68ac
acalvom <andreac.3108@gmail.com>
2023-12-31 18:58:24 +0100

    feat: add balance in Home page - wip

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,25 +1,43 @@
 import { Link } from 'react-router-dom'
 import { Layout } from '@/layout/Layout'
 import { AddItem } from '@/constants'
+import { sortExpenses, calculateBalance } from '@/utils'
 import { useLocalStorage } from '@/hooks/useLocalStorage'
-import { StoredExpense } from '@/models/Expense'
+import { StoredExpense, StoredUser } from '@/models'
 import { ExpenseCard } from '@/components/ExpenseCard/ExpenseCard'
-import { sortExpenses } from '@/utils/sortExpenses'
 import './Home.css'
 
 export const Home = () => {
   const [storedExpenses] = useLocalStorage<StoredExpense[]>('gastos', [])
+  const [storedUsers] = useLocalStorage<StoredUser[]>('amigos', [])
+
   const sortedExpenses = sortExpenses(storedExpenses)
+  const balance = calculateBalance(storedExpenses, storedUsers)
 
-
+  // TODO - split into components home-balance and home-expenses-list
   return (
     <Layout>
-      <div className="home-button" data-testid='add-home-button'>
+      <div className="home-button" data-testid="add-home-button">
         <Link to="/create" className="button">
           {AddItem}
         </Link>
       </div>
-      <div className="home-wrapper" data-testid="expenses-list">
+
+      {/* // TODO - Wip */}
+      <div className="home-balance" data-testid="balance">
+        <ul>
+          {Object.values(balance).map((balance) => (
+            <li key={balance.payer.id}>
+              <span>
+                {balance.payer.firstName} {balance.payer.lastName}
+              </span>{' '}
+              <span>Pagó {balance.paid}</span> <span>Debe {balance.accDebt}</span>{' '}
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="home-expenses-list" data-testid="expenses-list">
         {sortedExpenses.map((expese) => (
           <ExpenseCard key={expese.id} expense={expese} />
         ))}
