c552fed81553098586637769b542c717e3ca8199
acalvom <andreac.3108@gmail.com>
2024-04-09 15:25:16 +0200

    feat: add attribute to User entity

--- src/modules/users/domain/user.ts
+++ src/modules/users/domain/user.ts
@@ -1,19 +1,27 @@
+import { Datetime } from '@/shared/domain/dates/datetime'
 import { Id } from '@/shared/domain/interface/id'
 
 interface IUser {
   id: Id
   firstName: string
   lastName: string
+  birthDate: Date
 }
 
 export class User implements IUser {
   id: Id
   firstName: string
   lastName: string
+  birthDate: Date
 
   constructor(value: IUser) {
     this.id = value.id
     this.firstName = value.firstName
     this.lastName = value.lastName
+    this.birthDate = value.birthDate
+  }
+
+  public getAge(): number {
+    return Datetime.getYearsTillToday(this.birthDate)
   }
 }
---
+++ src/shared/domain/dates/datetime.ts
@@ -0,0 +1,6 @@
+// TODO: wip to be implemented
+export class Datetime {
+  static getYearsTillToday(date: Date): number {
+    throw new Error('Unimplemented method' + date)
+  }
+}
