d8df613f9f9e3ec8e52fec5238ab456b8a7e7ab5
acalvom <andreac.3108@gmail.com>
2024-04-09 17:00:20 +0200

    feat: set user in local storage

--- src/modules/users/infrastructure/local-storage-user-repository.ts
+++ src/modules/users/infrastructure/local-storage-user-repository.ts
@@ -1,9 +1,22 @@
 import { User } from '../domain/user'
 import { UserRepository } from '../domain/user.repository'
 
+// FIXME: check this try catch. should it be here?
 export class LocalStorageUserRepository implements UserRepository {
   getAll(): Promise<User[]> {
-    const storedValue = localStorage.getItem('amigos')
-    return storedValue ? JSON.parse(storedValue) : []
+    try {
+      const storedValue = localStorage.getItem('amigos')
+      return storedValue ? JSON.parse(storedValue) : []
+    } catch (error) {
+      throw new Error(error as string)
+    }
+  }
+
+  add(user: User): Promise<void> {
+    try {
+      return Promise.resolve(localStorage.setItem('amigos', JSON.stringify(user)))
+    } catch (error) {
+      throw new Error(error as string)
+    }
   }
 }
