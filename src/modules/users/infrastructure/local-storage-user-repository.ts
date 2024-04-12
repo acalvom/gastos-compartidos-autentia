c004c6ad52cafb65908c7388344c7db16c69b322
acalvom <andreac.3108@gmail.com>
2024-04-12 13:59:35 +0200

    fix: async function & remove Promise.resolve

--- src/modules/users/infrastructure/local-storage-user-repository.ts
+++ src/modules/users/infrastructure/local-storage-user-repository.ts
@@ -1,4 +1,4 @@
-import { AddUserDto } from '../application/dtos/add-user.dto'
+import { NewUser } from '../domain/new-user'
 import { User } from '../domain/user'
 import { UserRepository } from '../domain/user.repository'
 
@@ -12,22 +12,13 @@ export class LocalStorageUserRepository implements UserRepository {
     localStorage.setItem('users', JSON.stringify(users))
   }
 
-  // ASKME: check this try catch. should it be here?
-  getAll(): Promise<User[]> {
-    try {
-      return Promise.resolve(this.getUsersFromLocalStorage())
-    } catch (error) {
-      throw new Error(error as string)
-    }
+  async getAll(): Promise<User[]> {
+    return this.getUsersFromLocalStorage()
   }
 
-  add(user: AddUserDto): Promise<void> {
-    try {
-      const users = this.getUsersFromLocalStorage()
-      const newUser = { ...user, id: user.firstName + user.lastName }
-      return Promise.resolve(this.saveUsersToLocalStorage([...users, newUser]))
-    } catch (error) {
-      throw new Error(error as string)
-    }
+  async add(user: NewUser): Promise<void> {
+    const users = this.getUsersFromLocalStorage()
+    const newUser = { ...user, id: user.firstName + user.lastName }
+    this.saveUsersToLocalStorage([...users, newUser])
   }
 }
