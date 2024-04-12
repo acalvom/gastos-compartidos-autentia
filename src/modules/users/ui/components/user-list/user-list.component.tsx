15fdbc01efa569802569a5a7bd1e2d2784d2e6e0
acalvom <andreac.3108@gmail.com>
2024-04-12 14:37:21 +0200

    style: add styles to users module

--- src/modules/users/ui/components/user-list/user-list.component.tsx
+++ src/modules/users/ui/components/user-list/user-list.component.tsx
@@ -1,4 +1,5 @@
 import { useListUsers } from '../../controllers/use-list-users.hook'
+import './user-list.styles.css'
 
 export const UserList = () => {
   // TODO: enhance styles
@@ -6,7 +7,7 @@ export const UserList = () => {
   return (
     <div className="home-users">
       <h2 className="home-title">Usuarios</h2>
-      <ul className="home-user-list">
+      <ul className="user-list">
         {users.map(({ firstName, lastName }) => (
           <li>
             {firstName} {lastName}
