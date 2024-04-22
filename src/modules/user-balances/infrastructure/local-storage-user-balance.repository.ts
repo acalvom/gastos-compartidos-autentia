e775db89156a358848d2b4158efab89aeacf64d4
acalvom <andreac.3108@gmail.com>
2024-04-22 13:46:35 +0200

    feat: simulate api returned payload

--- src/modules/user-balances/infrastructure/local-storage-user-balance.repository.ts
+++ src/modules/user-balances/infrastructure/local-storage-user-balance.repository.ts
@@ -3,6 +3,7 @@ import { Id } from '@/shared/domain/interface/id'
 import { User } from '../domain/user'
 import { UserBalance } from '../domain/user-balance'
 import { UserBalanceRepository } from '../domain/user-balance.repository'
+import { usersTotalExpenseToUserBalanceMapper } from './mappers/user-balance.mapper'
 
 interface IExpensePrimitives {
   id: Id
@@ -12,52 +13,40 @@ interface IExpensePrimitives {
   paymentDate: string
 }
 
-type UserExpenses = Record<Id, number>
-
-interface GlobalBalance {
-  globalDebt: number
-  totalUsers: number
-  userExpenses: UserExpenses
+export type UserTotalExpense = {
+  user: User
+  totalExpense: number
 }
+export type UsersTotalExpense = Record<Id, UserTotalExpense>
+
 export class LocalStorageUserBalanceRepository implements UserBalanceRepository {
-  /**
-  // INFO: tengo que simular un array de UserBalances para ello, necesito implementer la siguiente lógica (que vendría dada del BE)
-   - obtener expenses
-   - agrupar expenses por id de payer
-   - obtener el sumatorio del gasto, el número de payers y el gasto por payer 
-   - obtener usuarios
-   - vincular usuarios a expenses (via payer)
-   - asociar el User del UserBalance 
-   - calcular la deuda por usuario
-   - montar el array Balance[]
-
-   Eso es lo que devuelvo
-  */
-
-  private getUsersExpensesFromLocalStorage() {
+  private getExpensesFromLocalStorage(): IExpensePrimitives[] {
     const expensesString = localStorage.getItem('expenses')
-    const jsonExpenses: IExpensePrimitives[] = expensesString ? JSON.parse(expensesString) : []
-    const globalExpense = jsonExpenses.reduce(
-      (accDebt, currExpense) => accDebt + currExpense.amount,
-      0
-    )
-    console.log('🎨 ☞ wip:', globalExpense)
+    return expensesString ? JSON.parse(expensesString) : []
+  }
 
-    const tt = jsonExpenses.reduce((acc: UserExpenses, jsonExpense: IExpensePrimitives) => {
+  private getUsersTotalExpenseFromLocalStorage(): UsersTotalExpense {
+    const jsonExpenses = this.getExpensesFromLocalStorage()
+    return jsonExpenses.reduce((acc: UsersTotalExpense, jsonExpense: IExpensePrimitives) => {
       if (!acc[jsonExpense.payer.id]) {
-        // No existen gastos asociados a ese usuario aun. Añadir la key y el gasto
-        return { ...acc, [jsonExpense.payer.id]: jsonExpense.amount }
+        return {
+          ...acc,
+          [jsonExpense.payer.id]: { user: jsonExpense.payer, totalExpense: jsonExpense.amount },
+        }
       } else {
-        // Ya existe algún gasto asociado a ese usuario. Añadir el gasto a los que ya existen
-        return { ...acc, [jsonExpense.payer.id]: acc[jsonExpense.payer.id] + jsonExpense.amount }
+        return {
+          ...acc,
+          [jsonExpense.payer.id]: {
+            user: acc[jsonExpense.payer.id].user,
+            totalExpense: acc[jsonExpense.payer.id].totalExpense + jsonExpense.amount,
+          },
+        }
       }
     }, {})
-
-    return tt
   }
 
   async getAll(): Promise<UserBalance[]> {
-    this.getUsersExpensesFromLocalStorage()
-    return []
+    const usersTotalExpense = this.getUsersTotalExpenseFromLocalStorage()
+    return usersTotalExpenseToUserBalanceMapper(usersTotalExpense)
   }
 }
---
+++ src/modules/user-balances/infrastructure/mappers/user-balance.mapper.ts
@@ -0,0 +1,17 @@
+import { UserBalance } from '../../domain/user-balance'
+import { UserTotalExpense, UsersTotalExpense } from '../local-storage-user-balance.repository'
+
+export function usersTotalExpenseToUserBalanceMapper(
+  usersTotalExpense: UsersTotalExpense
+): UserBalance[] {
+  const usersTotalExpenseArray = Object.values(usersTotalExpense)
+  const globalDebt = usersTotalExpenseArray.reduce(
+    (accDebt, currExpense) => accDebt + currExpense.totalExpense,
+    0
+  )
+  const userDebt = globalDebt / usersTotalExpenseArray.length
+  return usersTotalExpenseArray.map((item: UserTotalExpense) => ({
+    user: item.user,
+    debtAmount: item.totalExpense - userDebt,
+  }))
+}
