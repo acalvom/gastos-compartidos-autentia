db552ed390dd220ce68d9c2c386dbaad9dd89ba0
acalvom <andreac.3108@gmail.com>
2024-04-23 10:41:30 +0200

    style: add custom list style

--- src/modules/users/ui/components/user-list/user-list.component.tsx
+++ src/modules/users/ui/components/user-list/user-list.component.tsx
@@ -2,11 +2,10 @@ import { useUserList } from '../../controllers/use-user-list.hook'
 import './user-list.styles.css'
 
 export const UserList = () => {
-  // TODO: enhance styles
   const { users } = useUserList()
   return (
     <div className="home-users">
-      <h2 className="home-title">Usuarios</h2>
+      <h2 className="home-title">👯 Usuarios</h2>
       <ul className="user-list">
         {users.map(({ firstName, lastName, id }) => (
           <li key={id}>
