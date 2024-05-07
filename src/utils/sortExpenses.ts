eca44405ede07aa0a423b8a7c3dd59fe509bfec0
acalvom <andreac.3108@gmail.com>
2024-05-07 16:16:16 +0200

    chore: remove dead code

--- src/utils/sortExpenses.ts
+++
@@ -1,8 +0,0 @@
-import { StoredExpense } from '@/models'
-
-export const sortExpenses = (expenses: StoredExpense[]) => {
-  const compareDates = (a: StoredExpense, b: StoredExpense) =>
-    new Date(b.paymentDate).getTime() - new Date(a.paymentDate).getTime()
-
-  return [...expenses].sort(compareDates)
-}
